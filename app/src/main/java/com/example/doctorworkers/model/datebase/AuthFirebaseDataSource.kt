fd82c3f048c84bdf0c81328fa4be741da0e5f7d5
syoma <skat.apps@gmail.com>
2022-07-05 19:45:33 +0300

    create AuthFirebaseDataSource.kt

---
+++ app/src/main/java/com/example/doctorworkers/model/datebase/AuthFirebaseDataSource.kt
@@ -0,0 +1,42 @@
+package com.example.doctorworkers.model.datebase
+
+import com.example.doctorworkers.model.entities.Doctor
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+object AuthFirebaseDataSource {
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val firestore = FirebaseFirestore.getInstance()
+    private val ioDispatcher = Dispatchers.IO
+
+    fun getUser() = auth.currentUser
+
+    suspend fun signOut() = withContext(ioDispatcher) {
+        auth.signOut()
+    }
+
+    suspend fun signIn(
+        email: String,
+        password: String
+    ) = withContext(ioDispatcher) {
+        return@withContext auth.signInWithEmailAndPassword(email, password)
+    }
+
+    suspend fun createUser(
+        email: String,
+        password: String,
+    ) = withContext(ioDispatcher) {
+        return@withContext auth.createUserWithEmailAndPassword(email, password)
+    }
+
+    suspend fun addDoctorInDatabase(doctor: Doctor) = withContext(ioDispatcher) {
+        return@withContext firestore.collection("doctors").add(doctor)
+    }
+
+    suspend fun getWorkPassword() = withContext(ioDispatcher) {
+        return@withContext firestore.collection("information").document("isDoctor").get()
+    }
+}
