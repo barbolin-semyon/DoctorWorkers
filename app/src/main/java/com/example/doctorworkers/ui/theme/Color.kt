565110ea990431220ff48eaa9186fa5e9c532eaa
syoma <skat.apps@gmail.com>
2022-07-05 13:18:39 +0300

    add colors in project

--- app/src/main/java/com/example/doctorworkers/ui/theme/Color.kt
+++ app/src/main/java/com/example/doctorworkers/ui/theme/Color.kt
@@ -2,7 +2,16 @@ package com.example.doctorworkers.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Blue200 = Color(0xFF0089BA)
+val Blue500 = Color(0xFF0081CF)
+val Blue700 = Color(0xFF2C73D2)
+
+val Orange200 = Color(0xFFFFA18E)
+val Orange700 = Color(0xFFFF9671)
+
+val white = Color(0xFFFFFFFF)
+val black = Color(0xFF000000)
+
+val Gray200 = Color(0xFFEFF4FA)
+val Gray500 = Color(0xFF2B2A2A)
+val Gray700 = Color(0xFF1D1D1D)
