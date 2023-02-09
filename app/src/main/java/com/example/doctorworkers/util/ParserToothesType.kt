457b672d03e28423b2442a336fd1c90343fe57fe
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-09 19:03:38 +0300

    Создание ParserToothesType.kt

---
+++ app/src/main/java/com/example/doctorworkers/util/ParserToothesType.kt
@@ -0,0 +1,18 @@
+package com.example.doctors.util
+
+import com.example.doctors.entities.Toothes
+
+fun parseListIdToListToothes(listId: List<String>): MutableList<Toothes> {
+    val toothes = mutableListOf<Toothes>()
+    listId.forEach { toothes.add(getToothById(it)) }
+    return toothes
+}
+
+private fun getToothById(toothId: String): Toothes {
+    return when(toothId) {
+        Toothes.BrokenTooth.id -> Toothes.BrokenTooth
+        Toothes.InWorkTooth.id -> Toothes.InWorkTooth
+        Toothes.HealthyTooth.id -> Toothes.HealthyTooth
+        else -> Toothes.СariesTooth
+    }
+}
