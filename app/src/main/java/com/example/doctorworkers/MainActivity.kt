c81cf8948f850e780e685e51a0ce9e04f585207e
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-11 20:13:22 +0300

    Оформление MainActivity.kt

--- app/src/main/java/com/example/doctorworkers/MainActivity.kt
+++ app/src/main/java/com/example/doctorworkers/MainActivity.kt
@@ -1,43 +1,57 @@
 package com.example.doctorworkers
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.doctorworkers.ui.theme.DoctorWorkersTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.doctorworkers.ui.navigation.MainNavHost
+import com.example.doctorworkers.ui.navigation.Screens
+import com.example.doctorworkers.ui.theme.Gray200
+import com.example.doctorworkers.ui.theme.MaterialThemeDoctor
+import com.example.doctorworkers.viewModel.AuthViewModel
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DoctorWorkersTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+            val scaffoldState = rememberScaffoldState()
+
+            val viewModel: AuthViewModel = viewModel()
+            var startDestination = Screens.Auth.route
+
+            if (viewModel.isAuthorization()) {
+                viewModel.signOut()
+                startDestination = Screens.Main.route
+            }
+
+            val navController = rememberNavController()
+
+            MaterialThemeDoctor() {
+                Scaffold(
+                    bottomBar = {
+                        /*BottomNavigationDoctor(
+                            navController = navController
+                        )*/
+                    },
+                    scaffoldState = scaffoldState
                 ) {
-                    Greeting("Android")
+                    val ui = rememberSystemUiController()
+                    ui.setStatusBarColor(Gray200)
+
+                    MainNavHost(
+                        navController = navController,
+                        startDestination = startDestination,
+                        scaffoldState = scaffoldState
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DoctorWorkersTheme {
-        Greeting("Android")
     }
 }
