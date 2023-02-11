61bdcadf3a40c7ac52904f5ca39a1b31af96e60a
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-11 12:03:36 +0300

    Добавление поля

--- app/src/main/java/com/example/doctorworkers/model/entities/Doctor.kt
+++ app/src/main/java/com/example/doctorworkers/model/entities/Doctor.kt
@@ -8,6 +8,7 @@ data class Doctor(
     val id: String = "",    
     val avaragePrice: Int = 0,
     val name: String = "",
+    val countPeopleForRating: Int = 0,
     val rating: Double = 0.0,
     val urlAvatar: String = ""
 ) : Parcelable
