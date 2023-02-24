e502fd72a6e87b2ca6b9b0919fbbf89ce70a195b
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-24 21:19:26 +0300

    Создание PlacesFirebaseDataSource.kt для работы с данными по коммуникации с пациентами

---
+++ app/src/main/java/com/example/doctorworkers/model/datebase/PlacesFirebaseDataSource.kt
@@ -0,0 +1,31 @@
+package com.example.doctorworkers.model.datebase
+
+import com.example.doctors.entities.History
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+object PlacesFirebaseDataSource {
+    private val firestore = FirebaseFirestore.getInstance()
+    private val dispatcher = Dispatchers.Default
+
+    fun getQueryPlaces(doctor: String, year: Int, month: Int, day: Int): Query {
+        return firestore
+            .collection("doctors").document(doctor)
+            .collection("places").whereEqualTo("year", year)
+            .whereEqualTo("month", month).whereEqualTo("day", day)
+    }
+
+    suspend fun getPatientInfo(userId: String): Task<DocumentSnapshot> = withContext(dispatcher) {
+        return@withContext firestore.collection("users").document(userId).get()
+    }
+
+    suspend fun setHistory(userId: String, history: History) = withContext(dispatcher) {
+        return@withContext firestore
+            .collection("users").document(userId)
+            .collection("history").document(history.id).set(history)
+    }
+}
