213b05e285bc2b6ef9f77d77cded56590c0c02fd
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-25 16:34:27 +0300

    Добавление цвета

--- app/src/main/java/com/example/doctorworkers/ui/theme/Color.kt
+++ app/src/main/java/com/example/doctorworkers/ui/theme/Color.kt
@@ -6,6 +6,8 @@ val Blue200 = Color(0xFF0089BA)
 val Blue500 = Color(0xFF0081CF)
 val Blue700 = Color(0xFF2C73D2)
 
+val Green = Color(0xFF66FF9E)
+
 val Orange200 = Color(0xFFFFA18E)
 val Orange700 = Color(0xFFFF9671)
 
